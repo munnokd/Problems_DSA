a6b0033fe3cc86a7c78cd99b082127bd47e7f297
Kalp <kalp2002prajapati@gmail.com>
2023-02-01 22:03:08 +0530

    01-02-2023 leetcode solution

---
+++ 1071.Greatest_Common_Divisor_of_Strings.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String gcdOfStrings(String str1, String str2) {
+        if(str1.length() < str2.length()){
+            return gcdOfStrings(str2,str1);
+        }else if(!str1.startsWith(str2)){
+            return "";
+        }else if(str2.length() == 0){
+            return str1;
+        }else{
+            return gcdOfStrings(str1.substring(str2.length()),str2);
+        }
+    }
+}
