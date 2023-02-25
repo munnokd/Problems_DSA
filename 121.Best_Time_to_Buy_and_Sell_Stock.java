050c0ecd29e0af39cb7778be8198c307389bbcf5
Kalp <kalp2002prajapati@gmail.com>
2023-02-25 21:57:20 +0530

    25-02-2023 leetcode solution

---
+++ 121.Best_Time_to_Buy_and_Sell_Stock.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int min=Integer.MAX_VALUE;
+        int profit=0;
+        for(int i=0;i<prices.length;i++){
+            if(prices[i]<min){
+                min=prices[i];
+            }
+            else if(prices[i]-min>profit)
+                profit=prices[i]-min;
+        }
+        return profit;
+    }
+}
