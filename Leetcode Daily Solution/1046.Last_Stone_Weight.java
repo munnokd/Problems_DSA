df84786104539a746807a3fe1df21b29f0c147d8
Kalp Prajapati <81804004+munnokd@users.noreply.github.com>
2023-04-24 09:27:17 +0530

    24-04-2023 Leetcode solution

---
+++ Leetcode Daily Solution/1046.Last_Stone_Weight.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        int n = stones.length;
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        for (int stone : stones) {
+            maxHeap.offer(stone);
+        }
+        while (maxHeap.size() != 1) {
+            int stone1 = maxHeap.poll();
+            int stone2 = maxHeap.poll();
+            maxHeap.offer(Math.abs(stone1 - stone2));
+        }
+        return maxHeap.poll();
+    }
+}
