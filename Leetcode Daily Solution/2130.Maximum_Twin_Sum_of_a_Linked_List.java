0bedb7c5feaf25f7db26b8793200fe7f021fe781
Kalp Prajapati <81804004+munnokd@users.noreply.github.com>
2023-05-17 11:40:42 +0530

    17-05-2023 Leetcode solution

---
+++ Leetcode Daily Solution/2130.Maximum_Twin_Sum_of_a_Linked_List.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public int pairSum(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        int maxVal = 0;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode nextNode, prev = null;
+        while (slow != null) {
+            nextNode = slow.next;
+            slow.next = prev;
+            prev = slow;
+            slow = nextNode;
+        }
+
+        while (prev != null) {
+            maxVal = Math.max(maxVal, head.val + prev.val);
+            prev = prev.next;
+            head = head.next;
+        }
+
+        return maxVal;
+    }
+}
