1a7ea8c88809d29c97f5a844d8961fcf5d03bda2
Kalp Prajapati <81804004+munnokd@users.noreply.github.com>
2023-05-30 13:08:06 +0530

    30-05-2023 Leetcode solution

---
+++ Leetcode Daily Solution/705.Design_HashSet.java
@@ -0,0 +1,20 @@
+class MyHashSet {
+    private boolean[] mp;
+
+    public MyHashSet() {
+        mp = new boolean[1000001];
+        Arrays.fill(mp, false);
+    }
+
+    public void add(int key) {
+        mp[key] = true;
+    }
+
+    public void remove(int key) {
+        mp[key] = false;
+    }
+
+    public boolean contains(int key) {
+        return mp[key];
+    }
+}
