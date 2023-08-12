3f8caf73624e5be8df8c00d19d3407f74104b40c
Kalp Prajapati <81804004+munnokd@users.noreply.github.com>
2023-08-12 21:19:39 +0530

    12-08-2023 Leetcode Solution

---
+++ Leetcode Daily Solution/63.Unique_Paths_II.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int uniquePathsWithObstacles(int[][] OG) {
+        if (OG[0][0] == 1) return 0;
+        int m = OG.length, n = OG[0].length;
+        int[][] dp = new int[m][n];
+        dp[0][0] = 1;
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++)
+                if (OG[i][j] == 1 || (i == 0 && j == 0)) continue;
+                else dp[i][j] = (i > 0 ? dp[i-1][j] : 0) + (j > 0 ? dp[i][j-1] : 0);
+        return dp[m-1][n-1];
+    }
+}
